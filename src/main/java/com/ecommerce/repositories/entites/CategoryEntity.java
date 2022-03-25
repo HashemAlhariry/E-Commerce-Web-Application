8d53669ad2094197c33498b031aedc5a83a0057e
Ahmad Ashrf <ahmdashrf0097@gmail.com>
2022-03-25 17:15:34 +0200

    adding table name in CategoryEntity

--- src/main/java/com/ecommerce/repositories/entites/CategoryEntity.java
+++ src/main/java/com/ecommerce/repositories/entites/CategoryEntity.java
@@ -1,11 +1,9 @@
 package com.ecommerce.repositories.entites;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "category")
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
