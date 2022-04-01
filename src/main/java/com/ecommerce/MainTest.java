c647c9cf7fd30200283cca6c5f88f820c0a0471f
AhmedOsama <ahmedosamam03@gmail.com>
2022-04-01 14:42:07 +0200

    erd done

--- src/main/java/com/ecommerce/MainTest.java
+++ src/main/java/com/ecommerce/MainTest.java
@@ -1,18 +1,16 @@
 package com.ecommerce;
 
 import com.ecommerce.repositories.ProductRepository;
-import com.ecommerce.repositories.entites.Image;
 import com.ecommerce.repositories.entites.ProductEntity;
 import com.ecommerce.repositories.entites.Role;
 import com.ecommerce.repositories.entites.UserEntity;
 import com.ecommerce.repositories.impl.ProductRepositoryImpl;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.Set;
 
 public class MainTest {
@@ -21,14 +19,15 @@ public class MainTest {
         ProductRepository productRepository = ProductRepositoryImpl.getInstance();
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("amazonya");
         EntityManager em = emf.createEntityManager();
-        Image image1 = new Image("imagepath1");
-        UserEntity userEntity = new UserEntity("osos","hashem@osos","marwa","shoubra el kheima",new Date(), Role.CUSTOMER, BigDecimal.valueOf(2000L));
+//        Image image1 = new Image("imagepath1");
+        UserEntity userEntity = new UserEntity("osos","hashem@osos","marwa","shoubra el kheima", LocalDate.now(), Role.CUSTOMER, BigDecimal.valueOf(2000L));
 
 
-        ProductEntity product = new ProductEntity("Dell", 20000 , 3, "ana laptop osama", Set.of(image1));
-        image1.setProduct(product);
+        ProductEntity product = new ProductEntity("Dell", 20000 , 3, "ana laptop osama", Set.of("image1"));
+//        image1.setProduct(product);
         em.getTransaction().begin();
         em.persist(userEntity);
+        em.persist(product);
         em.getTransaction().commit();
         em.clear();
         em.close();
---
+++ src/main/java/com/ecommerce/repositories/entites/OrderState.java
@@ -0,0 +1,8 @@
+package com.ecommerce.repositories.entites;
+
+public enum OrderState {
+    PENDING,
+    PROCESSING,
+    ARRIVED,
+    CANCELLED
+}
