51eac520c3faf6a910ad3dbd6f9a28470cba30f8
A.Khattab <abdokhattab5@gmail.com>
2022-04-01 18:22:32 +0200

    Add controller for admin (Home , Product view , edit product) :)

---
+++ src/main/java/com/ecommerce/presentation/controllers/AdminProductView.java
@@ -0,0 +1,37 @@
+package com.ecommerce.presentation.controllers;
+
+import com.ecommerce.utils.CommonString;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "admin-product-view", urlPatterns = {"/admin-product"})
+
+public class AdminProductView extends HttpServlet {
+
+    private ServletContext servletContext;
+
+    @Override
+    public void init(ServletConfig config) {
+        servletContext = config.getServletContext();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher(CommonString.HOME_URL +"admin/pages/tables/products_view.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
