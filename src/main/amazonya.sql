90d282a57b5d9f484fa4bd4f939cddebda431428
AhmedOsama <ahmedosamam03@gmail.com>
2022-04-04 17:58:18 +0200

    add amazonya.sql

---
+++ src/main/amazonya.sql
@@ -0,0 +1,249 @@
+-- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: amazonya
+-- ------------------------------------------------------
+-- Server version	8.0.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `product_id` bigint NOT NULL,
+  `user_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`product_id`,`user_id`),
+  KEY `FKl70asp4l4w0jmbm1tqyofho4o` (`user_id`),
+  CONSTRAINT `FKl70asp4l4w0jmbm1tqyofho4o` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKpu4bcbluhsxagirmbdn7dilm5` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `categoryId` int NOT NULL,
+  `categoryImage` varchar(255) DEFAULT NULL,
+  `categoryName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'assets/images/demos/demo-4/cats/1.png','Computer & Laptop'),(2,'assets/images/demos/demo-4/cats/6.png','Smart Watches'),(3,'assets/images/demos/demo-4/cats/3.png','Smart Phones');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (4);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_details`
+--
+
+DROP TABLE IF EXISTS `order_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_details` (
+  `order_id` int NOT NULL,
+  `product_id` bigint NOT NULL,
+  `user_id` int NOT NULL,
+  `price` decimal(19,2) DEFAULT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`order_id`,`product_id`,`user_id`),
+  KEY `FK4q98utpd73imf4yhttm3w0eax` (`product_id`),
+  KEY `FKosrffjv6k8mr7ipsmmq7o7u1e` (`user_id`),
+  CONSTRAINT `FK4q98utpd73imf4yhttm3w0eax` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `FKjyu2qbqt8gnvno9oe9j2s2ldk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `FKosrffjv6k8mr7ipsmmq7o7u1e` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_details`
+--
+
+LOCK TABLES `order_details` WRITE;
+/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) DEFAULT NULL,
+  `dateTime` datetime(6) DEFAULT NULL,
+  `phoneNumber` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `totalPrice` decimal(19,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_images`
+--
+
+DROP TABLE IF EXISTS `product_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_images` (
+  `product_id` bigint NOT NULL,
+  `images` varchar(255) DEFAULT NULL,
+  KEY `FKqnq71xsohugpqwf3c9gxmsuy` (`product_id`),
+  CONSTRAINT `FKqnq71xsohugpqwf3c9gxmsuy` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_images`
+--
+
+LOCK TABLES `product_images` WRITE;
+/*!40000 ALTER TABLE `product_images` DISABLE KEYS */;
+INSERT INTO `product_images` VALUES (1,'https://amazonya.s3.eu-central-1.amazonaws.com/85af5262-a2dc-4493-9bb5-bdb17fc173ae3-Dell_Vostro_3510.5_lc8n-30.png_8bfa-tx_Easy-Resize.com_h1jx-00.jpg'),(1,'https://amazonya.s3.eu-central-1.amazonaws.com/1887c6a5-85a2-4de1-8486-56bf3770adfd1-Dell_Vostro_3510_Easy-Resize.com_4hkc-zl.jpg_0hhp-m4_Easy-Resize.com__2__w5lb-sh.jpg'),(1,'https://amazonya.s3.eu-central-1.amazonaws.com/ffbda54c-7418-41ec-88dd-68c0d8c93d742-dell-notebook-vostro-156-intel-core-i7-1165g7-8-gb-ram-1tb-black-vostro-15-3510-i7.jpg'),(2,'https://amazonya.s3.eu-central-1.amazonaws.com/218ebc21-1b95-4be9-8e1e-f55c0a9743272-hpzh939-4_1.jpg'),(2,'https://amazonya.s3.eu-central-1.amazonaws.com/13940c6a-8378-495e-b77e-685f8648f2641-hp250-g8-01-500x500.jpg'),(3,'https://amazonya.s3.eu-central-1.amazonaws.com/60887f0d-0148-4ac2-9700-a60ffa573b7e2-xiaomi41F7qu-NKdL._AC_SY741_.jpg'),(3,'https://amazonya.s3.eu-central-1.amazonaws.com/de586cea-6cc6-4706-9464-a6aeedbf8bea1-xiaomi41F7qu-NKdL._AC_SY741_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/e3b39964-7493-4ea2-ab01-a45ccdae10831-apple61i8Vjb17SL._AC_SX679_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/2c9c144c-651e-480b-9d4d-724759d46af12-apple61i8Vjb17SL._AC_SX679_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/cca48227-4e75-4760-b056-7228aeef50663-apple61i8Vjb17SL._AC_SX679_.jpg');
+/*!40000 ALTER TABLE `product_images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `creationDate` date DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(19,2) NOT NULL,
+  `quantity` int NOT NULL,
+  `rating` double NOT NULL,
+  `salePercentage` double NOT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `totalPurchasesNumber` int NOT NULL,
+  `category_categoryId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKdxwn3lf5fgvapvxdyyuf2mtuu` (`category_categoryId`),
+  CONSTRAINT `FKdxwn3lf5fgvapvxdyyuf2mtuu` FOREIGN KEY (`category_categoryId`) REFERENCES `category` (`categoryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'2022-04-04','','Dell Vostro 3510',15400.00,12,0,0,'NEW',0,1),(2,'2022-04-04','Intel Core i3-1005G1 4GB 1TB HDD INTEL GRAPHICS 15.6\" HD DOS Black','HP 250 G8',8249.00,3,0,0,'NEW',0,1),(3,'2022-04-04','9A Dual SIM Mobile Phone, 13 MP Camera, 6.53 Inch Touch Screen, 2 GB RAM, 32 GB, 4G LTE - Granite Grey','Xiaomi Redmi',2275.00,5,0,0,'NEW',0,3),(4,'2022-04-04','6.7-inch Super Retina XDR display with ProMotion for a faster, more responsive feel','Apple iPhone 13 Pro Max 256GB Blue',25399.00,6,0,0,'NEW',0,3);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) NOT NULL,
+  `creditLimit` decimal(19,2) NOT NULL,
+  `dateOfBirth` date DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-04 17:55:13
