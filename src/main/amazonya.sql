47501827cb84af4f3c39ef3f0dfae979c7073b41
Ahmad Ashrf <ahmdashrf0097@gmail.com>
2022-04-04 19:15:15 +0200

    database updated with five more records

--- src/main/amazonya.sql
+++ src/main/amazonya.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: amazonya
+-- Host: localhost    Database: amazonya
 -- ------------------------------------------------------
--- Server version	8.0.24
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -134,6 +134,7 @@ CREATE TABLE `orders` (
   `phoneNumber` varchar(255) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
   `totalPrice` decimal(19,2) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -168,7 +169,7 @@ CREATE TABLE `product_images` (
 
 LOCK TABLES `product_images` WRITE;
 /*!40000 ALTER TABLE `product_images` DISABLE KEYS */;
-INSERT INTO `product_images` VALUES (1,'https://amazonya.s3.eu-central-1.amazonaws.com/85af5262-a2dc-4493-9bb5-bdb17fc173ae3-Dell_Vostro_3510.5_lc8n-30.png_8bfa-tx_Easy-Resize.com_h1jx-00.jpg'),(1,'https://amazonya.s3.eu-central-1.amazonaws.com/1887c6a5-85a2-4de1-8486-56bf3770adfd1-Dell_Vostro_3510_Easy-Resize.com_4hkc-zl.jpg_0hhp-m4_Easy-Resize.com__2__w5lb-sh.jpg'),(1,'https://amazonya.s3.eu-central-1.amazonaws.com/ffbda54c-7418-41ec-88dd-68c0d8c93d742-dell-notebook-vostro-156-intel-core-i7-1165g7-8-gb-ram-1tb-black-vostro-15-3510-i7.jpg'),(2,'https://amazonya.s3.eu-central-1.amazonaws.com/218ebc21-1b95-4be9-8e1e-f55c0a9743272-hpzh939-4_1.jpg'),(2,'https://amazonya.s3.eu-central-1.amazonaws.com/13940c6a-8378-495e-b77e-685f8648f2641-hp250-g8-01-500x500.jpg'),(3,'https://amazonya.s3.eu-central-1.amazonaws.com/60887f0d-0148-4ac2-9700-a60ffa573b7e2-xiaomi41F7qu-NKdL._AC_SY741_.jpg'),(3,'https://amazonya.s3.eu-central-1.amazonaws.com/de586cea-6cc6-4706-9464-a6aeedbf8bea1-xiaomi41F7qu-NKdL._AC_SY741_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/e3b39964-7493-4ea2-ab01-a45ccdae10831-apple61i8Vjb17SL._AC_SX679_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/2c9c144c-651e-480b-9d4d-724759d46af12-apple61i8Vjb17SL._AC_SX679_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/cca48227-4e75-4760-b056-7228aeef50663-apple61i8Vjb17SL._AC_SX679_.jpg');
+INSERT INTO `product_images` VALUES (1,'https://amazonya.s3.eu-central-1.amazonaws.com/85af5262-a2dc-4493-9bb5-bdb17fc173ae3-Dell_Vostro_3510.5_lc8n-30.png_8bfa-tx_Easy-Resize.com_h1jx-00.jpg'),(1,'https://amazonya.s3.eu-central-1.amazonaws.com/1887c6a5-85a2-4de1-8486-56bf3770adfd1-Dell_Vostro_3510_Easy-Resize.com_4hkc-zl.jpg_0hhp-m4_Easy-Resize.com__2__w5lb-sh.jpg'),(1,'https://amazonya.s3.eu-central-1.amazonaws.com/ffbda54c-7418-41ec-88dd-68c0d8c93d742-dell-notebook-vostro-156-intel-core-i7-1165g7-8-gb-ram-1tb-black-vostro-15-3510-i7.jpg'),(2,'https://amazonya.s3.eu-central-1.amazonaws.com/218ebc21-1b95-4be9-8e1e-f55c0a9743272-hpzh939-4_1.jpg'),(2,'https://amazonya.s3.eu-central-1.amazonaws.com/13940c6a-8378-495e-b77e-685f8648f2641-hp250-g8-01-500x500.jpg'),(3,'https://amazonya.s3.eu-central-1.amazonaws.com/60887f0d-0148-4ac2-9700-a60ffa573b7e2-xiaomi41F7qu-NKdL._AC_SY741_.jpg'),(3,'https://amazonya.s3.eu-central-1.amazonaws.com/de586cea-6cc6-4706-9464-a6aeedbf8bea1-xiaomi41F7qu-NKdL._AC_SY741_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/e3b39964-7493-4ea2-ab01-a45ccdae10831-apple61i8Vjb17SL._AC_SX679_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/2c9c144c-651e-480b-9d4d-724759d46af12-apple61i8Vjb17SL._AC_SX679_.jpg'),(4,'https://amazonya.s3.eu-central-1.amazonaws.com/cca48227-4e75-4760-b056-7228aeef50663-apple61i8Vjb17SL._AC_SX679_.jpg'),(5,'https://amazonya.s3.eu-central-1.amazonaws.com/1fce2ed0-649c-4fda-98b8-90fb7901c4374_357_1.jpg'),(5,'https://amazonya.s3.eu-central-1.amazonaws.com/23e75623-e4a8-43a9-a04d-3ef861fcc1c22_463_1.jpg'),(5,'https://amazonya.s3.eu-central-1.amazonaws.com/03b11288-a99c-4f86-999e-ac47393245cdamazfit_watch_gts_2_-_midnight_black.jpg'),(6,'https://amazonya.s3.eu-central-1.amazonaws.com/73c112ea-24b5-4ded-aff0-028a2fa1044dsamsung-z-fold3--5g-sm-f926bds-_12_256_-phantom-green-3.jpg'),(6,'https://amazonya.s3.eu-central-1.amazonaws.com/883cb1ad-38b8-4573-ae70-661ea8ef1d1asamsung-z-fold3--5g-sm-f926bds-_12_256_-phantom-green-4.jpg'),(6,'https://amazonya.s3.eu-central-1.amazonaws.com/0761145a-9d80-4133-a463-d92c371bc85esamsung-z-fold3--5g-sm-f926bds-_12_256_-phantom-green-6.jpg'),(6,'https://amazonya.s3.eu-central-1.amazonaws.com/5a6270a3-ed19-4813-a0e2-1ef03f97e3f2samsung-z-fold3--5g-sm-f926bds-_12_256_-phantom-green-2.jpg'),(7,'https://amazonya.s3.eu-central-1.amazonaws.com/4ec41dd5-4e95-4d4f-8bf0-c39965d46fd36_165.jpg'),(7,'https://amazonya.s3.eu-central-1.amazonaws.com/aef03bab-90ea-42dd-a705-927fffc581611_405.jpg'),(8,'https://amazonya.s3.eu-central-1.amazonaws.com/f4de05f1-d03f-474a-acda-5d32528c164d6NW5N_4_Supersize.jpg.png'),(8,'https://amazonya.s3.eu-central-1.amazonaws.com/7a6ecd8c-0b19-4317-9d61-682aa380594en6503vn3501emea01_2105-2.jpg.png'),(8,'https://amazonya.s3.eu-central-1.amazonaws.com/f68f3334-e704-4fb5-99e5-c106cac6cf15LAPTOP_DELL_3501_EGYPTLAPTOP_317u-fs.jpg.png'),(9,'https://amazonya.s3.eu-central-1.amazonaws.com/3704eacd-1ba5-41c1-9d91-c103fb2fddc2lenovo-v155-15api-81v5-notebook-en_4_zyf5-0j.jpg.png'),(9,'https://amazonya.s3.eu-central-1.amazonaws.com/b24e7e84-545a-4b8d-ad0a-5647e8b81f8alenovo-v155-15api-81v5-notebook-en.jpg.png');
 /*!40000 ALTER TABLE `product_images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -194,7 +195,7 @@ CREATE TABLE `products` (
   PRIMARY KEY (`id`),
   KEY `FKdxwn3lf5fgvapvxdyyuf2mtuu` (`category_categoryId`),
   CONSTRAINT `FKdxwn3lf5fgvapvxdyyuf2mtuu` FOREIGN KEY (`category_categoryId`) REFERENCES `category` (`categoryId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -203,7 +204,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'2022-04-04','','Dell Vostro 3510',15400.00,12,0,0,'NEW',0,1),(2,'2022-04-04','Intel Core i3-1005G1 4GB 1TB HDD INTEL GRAPHICS 15.6\" HD DOS Black','HP 250 G8',8249.00,3,0,0,'NEW',0,1),(3,'2022-04-04','9A Dual SIM Mobile Phone, 13 MP Camera, 6.53 Inch Touch Screen, 2 GB RAM, 32 GB, 4G LTE - Granite Grey','Xiaomi Redmi',2275.00,5,0,0,'NEW',0,3),(4,'2022-04-04','6.7-inch Super Retina XDR display with ProMotion for a faster, more responsive feel','Apple iPhone 13 Pro Max 256GB Blue',25399.00,6,0,0,'NEW',0,3);
+INSERT INTO `products` VALUES (1,'2022-04-04','','Dell Vostro 3510',15400.00,12,0,0,'NEW',0,1),(2,'2022-04-04','Intel Core i3-1005G1 4GB 1TB HDD INTEL GRAPHICS 15.6\" HD DOS Black','HP 250 G8',8249.00,3,0,0,'NEW',0,1),(3,'2022-04-04','9A Dual SIM Mobile Phone, 13 MP Camera, 6.53 Inch Touch Screen, 2 GB RAM, 32 GB, 4G LTE - Granite Grey','Xiaomi Redmi',2275.00,5,0,0,'NEW',0,3),(4,'2022-04-04','6.7-inch Super Retina XDR display with ProMotion for a faster, more responsive feel','Apple iPhone 13 Pro Max 256GB Blue',25399.00,6,0,0,'NEW',0,3),(5,'2022-04-04','Brand: Amazfit\r\nModel Name: Watch GTS 2\r\nScreen: 1.65 inches AMOLED, 348 x 442\r\nBody: Aluminum alloy\r\nWaterproofing Grade:5 ATM\r\nBattery Capacity:246 mAh (typical value)\r\nCharging Time: About 2 hours','Amazfit Watch GTS 2 - Midnight Black',3199.00,2,0,0,'NEW',0,2),(6,'2022-04-04','Display Size In Inches: 7.6 inches\r\nProcessor CPU: Octa-core\r\nInternal Memory Capacity In GB: 256\r\nMemory RAM In GB: 12\r\nRear Camera:12 MP + 12 MP + 12 MP\r\nFront Camera: 4 MP + 10 MP\r\nBattery: 4400\r\n','Samsung Galaxy Z Fold3',32500.00,5,0,0,'NEW',0,3),(7,'2022-04-04','Display Size In Inches: 6.9 inches\r\nProcessor CPU: Octa-core\r\nInternal Memory Capacity In GB: 256 GB\r\nMemory RAM In GB: 8 GB\r\nRear Camera: 108 MP + 12 MP + 12 MP\r\nFront Camera: 10 MP\r\nBattery: 4500','Samsung Galaxy Note20 Ultra',19200.00,2,0,0,'NEW',0,3),(8,'2022-04-04','Amplified display: A brilliant FHD panel (optionally up to 220 nits) offers more brightness and vivid color for an enhanced front-of-screen experience, and a 2-sided narrow border ','DELL Vostro 3501',9000.00,12,0,0,'NEW',0,1),(9,'2022-04-04','LAPTOP LENOVO V155 AMD R3-3200U RAM 4GB HDD 1TB Integrated AMD Radeon GREY','LAPTOP LENOVO V155',9600.00,4,0,0,'NEW',0,1);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -246,4 +247,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-04 17:55:13
+-- Dump completed on 2022-04-04 19:13:52
