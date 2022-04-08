99c57a392ab69bb5df0adba038c76a351927c11b
AhmedOsama <ahmedosamam03@gmail.com>
2022-04-08 12:55:42 +0200

    fixed new arrival query

--- src/main/java/com/ecommerce/repositories/entites/ProductEntity.java
+++ src/main/java/com/ecommerce/repositories/entites/ProductEntity.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Entity
 @Table(name = "products")
 @NamedQueries({
-        @NamedQuery(name = "newArrivals",query = "select p from ProductEntity p where p.state not in  (com.ecommerce.repositories.entites.ProductState.ARCHIVED,com.ecommerce.repositories.entites.ProductState.OUT_OF_STOCK)  ORDER BY p.creationDate ,p.id DESC "),
+        @NamedQuery(name = "newArrivals",query = "select p from ProductEntity p where p.state not in  (com.ecommerce.repositories.entites.ProductState.ARCHIVED,com.ecommerce.repositories.entites.ProductState.OUT_OF_STOCK)  ORDER BY p.creationDate DESC,p.id DESC "),
         @NamedQuery(name = "findAllByCategoryId",query = "select p from ProductEntity p   where  p.category.id = :category_id"),
         @NamedQuery(name = "findProductByName",query = "select p from ProductEntity p   where  p.name  LIKE  CONCAT('%',:product_name,'%') ")
 
