f612f8132f86fb8ec941cdceb7ae813eab0e4ede
AhmedOsama <ahmedosamam03@gmail.com>
2022-04-10 12:59:52 +0200

    view users

--- src/main/java/com/ecommerce/utils/mappers/UserMapper.java
+++ src/main/java/com/ecommerce/utils/mappers/UserMapper.java
@@ -1,24 +1,41 @@
 package com.ecommerce.utils.mappers;
 
 import com.ecommerce.presentation.beans.LoginUserBean;
-import com.ecommerce.presentation.beans.ProductBean;
 import com.ecommerce.presentation.beans.SignUpBean;
-import com.ecommerce.repositories.entites.ProductEntity;
+import com.ecommerce.presentation.beans.UserViewBean;
+import com.ecommerce.repositories.entites.Role;
 import com.ecommerce.repositories.entites.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
+
 @Mapper
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     UserEntity userBeanToEntity(LoginUserBean userBean);
+
     LoginUserBean userEntityToBean(UserEntity userEntity);
 
 
     UserEntity userRegBeanToEntity(SignUpBean userBean);
+
     SignUpBean userRegEntityToBean(UserEntity userEntity);
+
+    @Mapping(source = "role", target = "role", qualifiedByName = "getRoleName")
+    UserViewBean userEntityToViewUserBean(UserEntity userEntity);
+
+    @Mapping(source = "role", target = "role", qualifiedByName = "getRoleName")
+    List<UserViewBean> userEntitiesToViewUserBeans(List<UserEntity> userEntities);
+
+    @Named("getRoleName")
+
+    default String getRoleName(Role role) {
+        return role.toString();
+    }
 
 }
--- src/main/webapp/WEB-INF/jsp/admin/includes/sidebar.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/includes/sidebar.jsp
@@ -25,7 +25,7 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="pages/tables/users.html">
+            <a class="nav-link" href="admin-view-users">
                 <i class="fas fa-minus-square menu-icon"></i>
                 <span class="menu-title">Users</span>
             </a>
