5a59f9e6d9d3a8932b8ee093e2a55c18611e334a
Ahmad Ashrf <ahmdashrf0097@gmail.com>
2022-04-16 02:11:40 +0200

    AuthorizationFilter class added without code

---
+++ src/main/java/com/ecommerce/handlers/AuthorizationFilter.java
@@ -0,0 +1,23 @@
+package com.ecommerce.handlers;
+
+import com.ecommerce.presentation.beans.UserBean;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class AuthorizationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        UserBean userBean = (UserBean) httpRequest.getSession().getAttribute("userBean");
+        if (userBean != null && userBean.getRole().equalsIgnoreCase("ADMIN") ) {
+            filterChain.doFilter(request, response);
+        } else {
+            request.setAttribute("errorMessage", "you dont have authority to view this page , please login as admin");
+            ((HttpServletResponse) response).sendRedirect("login?authorize=false");
+        }
+    }
+}
