b3db26e42dc9801db1380db9b340821ffa8ebae6
AhmedOsama <ahmedosamam03@gmail.com>
2022-04-18 17:51:06 +0200

    fix register bug

--- src/main/java/com/ecommerce/presentation/controllers/SignUpServlet.java
+++ src/main/java/com/ecommerce/presentation/controllers/SignUpServlet.java
@@ -1,15 +1,25 @@
 package com.ecommerce.presentation.controllers;
+
+import com.ecommerce.exceptions.FoundBeforeException;
+import com.ecommerce.presentation.beans.ResponseMessageBean;
 import com.ecommerce.presentation.beans.SignUpBean;
-import com.ecommerce.services.RegisterServices;
-import com.ecommerce.services.impls.RegisterServicesImpl;
+import com.ecommerce.services.UserService;
+import com.ecommerce.services.impls.UserServiceImpl;
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolationException;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 @WebServlet(name = "SignUp", urlPatterns = "/registration")
 public class SignUpServlet extends HttpServlet {
 
@@ -21,22 +31,32 @@ public class SignUpServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RegisterServices registerServicesImpl = new RegisterServicesImpl((String) req.getAttribute("reqId"));
-        String userName = req.getParameter("register-name");
-        String date = req.getParameter("register-birthday");
-        String email = req.getParameter("register-email");
-        String pass = req.getParameter("register-password");
-        String address = req.getParameter("register-address");
+        PrintWriter out = resp.getWriter();
+        UserService userService = new UserServiceImpl((String) req.getAttribute("reqId"));
+        List<String> messages = new ArrayList<>();
+        String userName = req.getParameter("register_name");
+        String date = req.getParameter("register_birthday");
+        String email = req.getParameter("register_email");
+        String pass = req.getParameter("register_password");
+        String address = req.getParameter("register_address");
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate dateOfBirth = LocalDate.parse(date, dateTimeFormatter);
-        SignUpBean userRegistrationBean = new SignUpBean(userName, dateOfBirth, email, pass, address);
-        if (userRegistrationBean == null) {
-            resp.sendRedirect("404.jsp");
-        } else {
-            SignUpBean signUpBean = registerServicesImpl.registerUser(userRegistrationBean);
-            // leh hna y set attribute 3l session d 3ml register msh 3amel login //TODO
-            req.getSession().setAttribute("userDto", signUpBean);
+
+        try {
+            SignUpBean userRegistrationBean = new SignUpBean(userName, dateOfBirth, email, pass, address);
+            userService.registerUser(userRegistrationBean);
+            messages.add("Created Successfully");
+            messages.add("Check mail to verify");
+            ResponseMessageBean responseMessageBean = new ResponseMessageBean("success", messages);
+            out.println(new Gson().toJson(responseMessageBean));
+        } catch (FoundBeforeException | ConstraintViolationException e) {
+            e.printStackTrace();
+            messages.add(e.getMessage());
+            ResponseMessageBean responseMessageBean = new ResponseMessageBean("failed", messages);
+            out.println(new Gson().toJson(responseMessageBean));
         }
+        // leh hna y set attribute 3l session d 3ml register msh 3amel login //TODO
+//            req.getSession().setAttribute("userDto", signUpBean);
 
         // SignUpBean signUpBean =
         // registerServicesImpl.registerUser(userRegistrationBean);
